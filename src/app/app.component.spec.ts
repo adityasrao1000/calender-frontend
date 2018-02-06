e63c33c505b0b7f1541a9e613fb43963e20d9c8c
Aditya Rao <adityasrao1000@gmail.com>
2018-02-06 14:22:57 +0530

    unit tests added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA }          from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
@@ -6,22 +7,26 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas:      [ NO_ERRORS_SCHEMA ]
     }).compileComponents();
   }));
+  
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  
+  it(`should not have title variable defined`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual(undefined);
   }));
-  it('should render title in a h1 tag', async(() => {
+  
+  it('value of nav-link one toContain "calendar"', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelectorAll('.nav-link')[0].textContent).toContain('calendar');
   }));
 });
